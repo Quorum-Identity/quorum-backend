2f2e85366afc496bde15f8f33c0ee49828106544
Nick <dev@labict.it>
2022-11-23 10:45:57 +0100

    Update product state

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { body } from 'express-validator';
-import { addProduct, getAllProducts, getProduct, getProductById } from "../services/product.service";
+import { addProduct, getAllProducts, getProduct, getProductById, updateProductState} from "../services/product.service";
 import { authorization } from "../middleware/user.midd";
 
 var router = express.Router();
@@ -21,5 +21,9 @@ router.post("/getbyid",
 getProductById);
 
 router.get("/getall", getAllProducts);
+router.post("/update/state", 
+  body('state').isNumeric(),
+  body('id').isLength({ min: 3 }), 
+updateProductState);
 
 export default router;
